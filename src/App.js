1473136a89923ca197d9326ad188819f923d3920
Fawaz1077 <107772207+Fawaz1077@users.noreply.github.com>
2023-02-12 13:47:47 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <>
+    <div className="card">
+      <img src="https://i.pinimg.com/564x/c4/81/c0/c481c067ad2e11fab13ffe39bc0fd975.jpg" alt="My handsome face"/>
+      <h2>Welcome</h2>
+      <p>I was tasked with making a card for this class. I decided to keep it simple and nice. I also decided to add my handsome face to it.</p>
+      <button className="cool-button">DETAILS</button>
     </div>
+      <button id="duplicate">DUPLICATION MACHINE</button>
+      <button id="change-color">COLOR CHANGING MACHINE</button>
+      <button id="change-text">TEXT CHANGING MACHINE</button>
+      <button class="delete">DELETING MACHINE</button> </>
   );
 }
 
